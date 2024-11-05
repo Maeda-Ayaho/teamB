262e0dd1f69ac29f12c845f66dc92287ceb4530b
Maeda <maeda.ayaho@technopro.com>
2024-11-05 09:57:48 +0900

    school_evaluationsの@table内を修正（直コミット許可済み）

--- demo/src/main/java/com/example/demo/model/School_evaluations.java
+++ demo/src/main/java/com/example/demo/model/School_evaluations.java
@@ -13,7 +13,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "School_evaluations")
+@Table(name = "school_evaluations")
 public class School_evaluations {
 
     @Id
